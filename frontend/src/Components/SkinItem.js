ee95dd8faadf89e39629ea90c2780df0c5521637
Georgia Bjärstål <georgia.bjarstal@epita.fr>
2023-11-11 22:54:42 +0100

    ADD skin shop

---
+++ frontend/src/Components/SkinItem.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import IconButton from '@mui/material/IconButton';
+
+export default function SkinItem(props) {
+    return (
+        <div className="skin-item" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+            <img src={props.image} alt={props.name} style={{ maxHeight: "32px" }} />
+            <p>{props.name} · {props.price} XP</p>
+            <IconButton aria-label="add to shopping cart">
+                <ShoppingCartIcon />
+            </IconButton>
+        </div >
+    );
+}
