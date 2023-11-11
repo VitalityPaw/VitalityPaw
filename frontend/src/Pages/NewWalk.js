69dd8fef192c6f577534ba4a164923197addfdb1
Georgia Bjärstål <georgia.bjarstal@epita.fr>
2023-11-11 22:01:49 +0100

    RM useless import

--- frontend/src/Pages/NewWalk.js
+++ frontend/src/Pages/NewWalk.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
