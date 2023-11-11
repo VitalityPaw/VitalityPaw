dc5e21e082d14d34df8b83af48f03e276ecda642
Stephane Gelibert <stephane.gelibert@epita.fr>
2023-11-11 16:12:07 +0100

    Add pet is now customizable

--- backend/src/routes/petRoutes.js
+++ backend/src/routes/petRoutes.js
@@ -5,10 +5,18 @@ const Pet = require('../models/pet');
 // Route to create a pet profile
 router.post('/addPet', async (req, res) => {
   try {
+    // Extract name and age from the request body
+    const { name, age } = req.body;
+
+    // Check if name and age are provided
+    if (!name || !age) {
+      return res.status(400).json({ success: false, message: 'Name and age are required.' });
+    }
+
     // Create a pet profile
     const profile = {
-      name: 'Pépite',
-      age: 4,
+      name,
+      age,
       XP: 0,
     };
 
