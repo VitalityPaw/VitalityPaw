89226239afa3ee69f1f6040a2af66d1a595bd788
Eric Bjarstal <ericbmousy@gmail.com>
2023-11-11 23:16:27 +0100

    ADD walker to walk history

--- frontend/src/Pages/MyWalks.js
+++ frontend/src/Pages/MyWalks.js
@@ -13,12 +13,12 @@ import itinary6 from "../Images/itinary6.png";
 
 export default function MyWalks() {
     let walks = [
-        ["Rocky", "13 nov. 2023", "12:00", "13:37", "1:37", "7,3 km", itinary1],
-        ["Rocky", "14 nov. 2023", "08:30", "09:45", "1:15", "5.5 km", itinary2],
-        ["Rocky", "15 nov. 2023", "15:00", "16:30", "1:30", "15 km", itinary3],
-        ["Rocky", "16 nov. 2023", "07:00", "09:00", "2:00", "10 km", itinary4],
-        ["Rocky", "17 nov. 2023", "14:00", "15:30", "1:30", "8 km", itinary5],
-        ["Rocky", "18 nov. 2023", "18:45", "19:30", "0:45", "3.7 km", itinary6]
+        ["Rocky", "12 nov. 2023", "12:00", "13:37", "1:37", "7,3 km", itinary1, "Georgia"],
+        ["Rocky", "11 nov. 2023", "08:30", "09:45", "1:15", "5,5 km", itinary2, "Stephane"],
+        ["Rocky", "11 nov. 2023", "15:00", "16:30", "1:30", "15 km", itinary3, "Rianando"],
+        ["Rocky", "11 nov. 2023", "07:00", "09:00", "2:00", "10 km", itinary4, "Rianando"],
+        ["Rocky", "10 nov. 2023", "14:00", "15:30", "1:30", "8 km", itinary5, "Georgia"],
+        ["Rocky", "10 nov. 2023", "18:45", "19:30", "0:45", "3,7 km", itinary6, "Eric"]
     ];
 
     return (
@@ -36,7 +36,7 @@ export default function MyWalks() {
             >
 
                 {walks.map((walk, index) => (
-                    <PastWalk key={index} dogName={walk[0]} walkDate={walk[1]} startHour={walk[2]} endHour={walk[3]} walkDuration={walk[4]} walkDistance={walk[5]} itinary={walk[6]} />
+                    <PastWalk key={index} dogName={walk[0]} walkDate={walk[1]} startHour={walk[2]} endHour={walk[3]} walkDuration={walk[4]} walkDistance={walk[5]} itinary={walk[6]} walker={walk[7]}/>
                 ))}
             </Box>
             <Footer />
