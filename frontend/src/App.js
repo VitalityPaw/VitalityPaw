5403dba50a8f331642575001696d6e38479e3b34
Georgia Bjärstål <georgia.bjarstal@epita.fr>
2023-11-11 13:34:14 +0100

    ADD main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,51 @@
+import AddIcon from '@mui/icons-material/Add';
+import DirectionsWalkIcon from '@mui/icons-material/DirectionsWalk';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import IconButton from '@mui/material/IconButton';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
 import MenuAppBar from "./Components/MenuAppBar";
+import oneDog from "./Images/oneDog.svg";
+import PeopleIcon from '@mui/icons-material/People';
+import Stack from '@mui/material/Stack';
 
 function App() {
   return (
-    <MenuAppBar />
+    <>
+      <MenuAppBar />
+      <div style={{
+        minHeight: "100vh", backgroundColor: "#FFF4EF"
+      }}>
+        <img src={oneDog} alt="logo" style={{
+          maxHeight: "40vh", display: "block", marginLeft: "auto", marginRight: "auto"
+        }}
+        />
+      </div>
+      <Stack
+        direction="row"
+        justifyContent="space-around"
+        alignItems="stretch"
+        spacing={2}
+        sx={{ backgroundColor: "#C6EAA5", borderRadius: "20px", margin: "10vw", position: "absolute", bottom: "0", left: "0", right: "0", textAlign: "center" }}
+
+      >
+        <IconButton aria-label="walk" size="large">
+          <DirectionsWalkIcon fontSize="inherit" />
+        </IconButton>
+        <IconButton aria-label="vet" size="large">
+          <LocalHospitalIcon fontSize="inherit" />
+        </IconButton>
+        <IconButton aria-label="people" size="large">
+          <PeopleIcon fontSize="inherit" />
+        </IconButton>
+        <IconButton aria-label="events" size="large">
+          <EmojiEventsIcon fontSize="inherit" />
+        </IconButton>
+        <IconButton aria-label="add" size="large">
+          <AddIcon fontSize="inherit" />
+        </IconButton>
+
+      </Stack>
+    </>
   );
 }
 
